71eca00d2e5e98f036334c2ef5752d99e124052e
w_robot <1281097064@qq.com>
2025-01-24 13:56:45 +0800

    examing learning-cxx

--- build/linux/x86_64/release/build/.deps/exercise33/linux/x86_64/release/exercise33.d
+++ build/linux/x86_64/release/build/.deps/exercise33/linux/x86_64/release/exercise33.d
@@ -1,12 +1,12 @@
 {
-    files = {
-        "../build/linux/x86_64/release/build/.objs/exercise33/linux/x86_64/release/33_std_accumulate/main.cpp.o"
-    },
     values = {
         "/usr/bin/g++",
         {
             "-m64",
             "-s"
         }
+    },
+    files = {
+        "../../../learning-cxx/build/linux/x86_64/release/build/.objs/exercise33/linux/x86_64/release/33_std_accumulate/main.cpp.o"
     }
 }
