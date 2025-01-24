2f51e281e1941414368dc617ad1c355968a73147
w_robot <1281097064@qq.com>
2025-01-24 13:39:13 +0800

    exam learn-cxx

---
+++ build/.deps/test/linux/x86_64/release/learn/test.cpp.o.d
@@ -0,0 +1,22 @@
+{
+    files = {
+        "learn/test.cpp"
+    },
+    values = {
+        "/usr/bin/gcc",
+        {
+            "-m64",
+            "-fvisibility=hidden",
+            "-fvisibility-inlines-hidden",
+            "-Wall",
+            "-O3",
+            "-std=c++17",
+            "-D__XMAKE__=\"/home/robot/workspace/AIwinterbootcamp/exam-grading/learning-cxx\"",
+            "-finput-charset=UTF-8",
+            "-fexec-charset=UTF-8",
+            "-DNDEBUG"
+        }
+    },
+    depfiles_gcc = "test.o: learn/test.cpp learn/test.h\
+"
+}
---
+++ build/linux/x86_64/release/build/.deps/exercise30/linux/x86_64/release/exercise30.d
@@ -0,0 +1,12 @@
+{
+    values = {
+        "/usr/bin/g++",
+        {
+            "-m64",
+            "-s"
+        }
+    },
+    files = {
+        "../build/linux/x86_64/release/build/.objs/exercise30/linux/x86_64/release/30_std_unique_ptr/main.cpp.o"
+    }
+}
